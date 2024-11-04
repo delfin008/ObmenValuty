e26d8062ca86bb549797f7c91c7c32c0c136fd8f
DELFIN <delfin000008@gmail.com>
2024-11-04 10:20:51 +0500

    Добавлен третий выпадающий список для выбора второй базовой валюты

--- ObmenValuty.py
+++ ObmenValuty.py
@@ -19,8 +19,9 @@ def update_t_label(event):
 def exchange():
     t_code = t_combobox.get()
     b_code = b_combobox.get()
+    b2_code = b2_combobox.get()
 
-    if t_code and b_code:
+    if t_code and b_code and b2_code:
         try:
             response = requests.get(f'https://open.er-api.com/v6/latest/{b_code}')
             response.raise_for_status()
@@ -29,9 +30,13 @@ def exchange():
                 exchange_rate = data['rates'][t_code]
                 t_name = curs[t_code]
                 b_name = curs[b_code]
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {t_name} за 1 {b_name}")
+                b2_name = curs[b2_code]
+                mb.showinfo("Курс обмена базовой валюты", f"Курс: {exchange_rate:.2f} {t_name} за 1 {b_name},")
+                mb.showinfo("Курс обмена второй базовой валюты", f"Курс: {exchange_rate:.2f} {t_name} за 1 {b2_name},")
             else:
                 mb.showerror("Ошибка!", f"Валюта {t_code} не найдена!")
+                mb.showerror("Ошибка!", f"Валюта {b_code} не найдена!")
+                mb.showerror("Ошибка!", f"Валюта {b2_code} не найдена!")
         except Exception as e:
             mb.showerror("Ошибка!", f"Произошла ошибка: {e}")
     else:
@@ -53,7 +58,7 @@ curs = {
 
 window = Tk()
 window.title("Курс обмена валют")
-window.geometry("360x300")
+window.geometry("360x400")
 
 Label(text="Базовая валюта").pack(padx=10, pady=10)
 
@@ -63,6 +68,14 @@ b_combobox.bind("<<ComboboxSelected>>", update_b_label)
 
 b_label = ttk.Label()
 b_label.pack(padx=10, pady=10)
+
+Label(text="Вторая базовая валюта").pack(padx=10, pady=10)
+
+b2_combobox = ttk.Combobox(values=list(curs.keys()))
+b2_combobox.pack(padx=10, pady=10)
+
+
+
 
 
 Label(text="Целевая валюта").pack(padx=10, pady=10)
